6b59cefac82c70fb1c8b453b3109cb9598e5412f
ElAmirr <othmaniamir41@gmail.com>
2022-06-04 15:17:35 +0100

    p

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './js/dashboard_main.js',
   output: {
-    filename: 'bundle',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
-  mode: 'development'
+  mode: "development"
 };
