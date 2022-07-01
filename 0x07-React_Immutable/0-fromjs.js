93cfc51975496b0215c68bbf4f0e25662d45b891
ElAmirr <othmaniamir41@gmail.com>
2022-07-01 11:23:16 +0100

    ppp

---
+++ 0x07-React_Immutable/0-fromjs.js
@@ -0,0 +1,14 @@
+import { Map } from "immutable";
+
+let object = Map({
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
+});
+
+function getImmutableObject(object){
+    return 
+}
+
+object = getImmutableObject(object);
