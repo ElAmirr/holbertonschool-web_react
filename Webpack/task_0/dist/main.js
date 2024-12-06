e4d2bd46153703f8ef8b3e58aa1769f601a942ff
ElAmirr <othmaniamir41@gmail.com>
2024-12-06 13:06:20 +0100

    push

--- Webpack/task_0/dist/main.js
+++
@@ -1,1 +0,0 @@
-$(document).ready((()=>{$("body").append("<p>Holberton Dashboard</p>"),$("body").append("<p>Dashboard data for the students</p>"),$("body").append("<p>Copyright - Holberton School</p>")}));
--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,5 +1,5 @@
-$(document).ready(() => {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
+import $ from "jquery";
+
+$("<p>Holberton Dashboard</p>").appendTo($('body'));
+$("<p>Dashboard data for the students</p>").appendTo($('body'));
+$("<p>Copyright - Holberton School</p>").appendTo($('body'));
