dfdeeb3e60bc4a55ef07314cc1420869cb8927fb
ElAmirr <othmaniamir41@gmail.com>
2024-12-06 13:17:27 +0100

    push

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,20 +1,7 @@
 import $ from "jquery";
-import _ from "lodash";
-import "../css/main.css";
+import "../css/main.css"; // Import the CSS file
 
-$(document).ready(() => {
-  $("body").append('<div id="logo"></div>');
-  $("body").append("<p>Holberton Dashboard</p>");
-  $("body").append("<p>Dashboard data for the students</p>");
-  $("body").append("<button>Click here to get started</button>");
-  $("body").append("<p id='count'></p>");
-  $("body").append("<p>Copyright - Holberton School</p>");
-
-  let count = 0;
-  const updateCounter = _.debounce(() => {
-    count++;
-    $("#count").text(`${count} clicks on the button`);
-  }, 500);
-
-  $("button").on("click", updateCounter);
-});
+// Add elements to the DOM
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<button id="counter">Click Me</button>');
--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,22 +1,40 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./js/dashboard_main.js",
+  mode: "production", // Set mode to production
+  entry: "./js/dashboard_main.js", // Entry point
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"), // Output folder
+    filename: "bundle.js", // Output JS file name
+    assetModuleFilename: "assets/[name][ext][query]", // Asset output folder
   },
-  mode: "production",
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/, // Process CSS files
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|jpeg|gif)$/i,
+        test: /\.(png|jpe?g|gif)$/i, // Process image files
         type: "asset/resource",
+        use: [
+          {
+            loader: "image-webpack-loader", // Optimize images
+            options: {
+              mozjpeg: { progressive: true, quality: 65 },
+              optipng: { enabled: true },
+              pngquant: { quality: [0.65, 0.9], speed: 4 },
+            },
+          },
+        ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // Template HTML
+      filename: "index.html", // Output HTML
+    }),
+  ],
 };
