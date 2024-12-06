435790ac82a3f255247daf7aeba99296b505380c
ElAmirr <othmaniamir41@gmail.com>
2024-12-06 20:07:19 +0100

    done

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,17 +1,7 @@
-import $ from 'jquery';
-const _ = require('lodash');
-import "../css/main.css";
+import $ from "jquery";
+import "../css/main.css"; // Import the CSS file
 
-$("<div id='logo'></div>").appendTo($("body"));
-$("<p>Holberton Dashboard</p>").appendTo($("body"));
-$("<p>Dashboard data for the students</p>").appendTo($("body"));
-$("<button>Click here to get started</button>").appendTo($("body"));
-$("<p id='count'></p>").appendTo($("body"));
-$("<p>Copyright - Holberton School</p>").appendTo($("body"));
-
-let counter = 0;
-const debouncedCounter = _.debounce(() => {
-    counter++;
-    $("#count").text(`${counter} clicks on the button`);
-});
-$('button').on('click', debouncedCounter);
+// Add elements to the DOM
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<button id="counter">Click Me</button>');
