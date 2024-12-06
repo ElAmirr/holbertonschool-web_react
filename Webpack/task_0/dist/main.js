28b253b93d1d965aa0f6f28d4568ec982c7d0c90
ElAmirr <othmaniamir41@gmail.com>
2024-12-06 12:29:48 +0100

    push

---
+++ Webpack/task_0/dist/main.js
@@ -0,0 +1,1 @@
+$(document).ready((()=>{$("body").append("<p>Holberton Dashboard</p>"),$("body").append("<p>Dashboard data for the students</p>"),$("body").append("<p>Copyright - Holberton School</p>")}));
