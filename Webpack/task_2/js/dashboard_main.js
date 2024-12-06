de14cb8b97e487c9cb223429459eae9f498b2c6f
ElAmirr <othmaniamir41@gmail.com>
2024-12-06 13:36:20 +0100

    push

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,7 +1,18 @@
 import $ from "jquery";
-import "../css/main.css"; // Import the CSS file
+import _ from "lodash";
 
-// Add elements to the DOM
-$('body').append('<div id="logo"></div>');
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<button id="counter">Click Me</button>');
+$(document).ready(() => {
+  $("body").append("<p>Holberton Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  $("body").append("<button>Click here to get started</button>");
+  $("body").append("<p id='count'></p>");
+  $("body").append("<p>Copyright - Holberton School</p>");
+
+  let count = 0;
+  const updateCounter = _.debounce(() => {
+    count++;
+    $("#count").text(`${count} clicks on the button`);
+  }, 500);
+
+  $("button").on("click", updateCounter);
+});
